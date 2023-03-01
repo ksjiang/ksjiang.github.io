fff6293e437b0088f1e0253b43ae9b4c51f46e15
Kyle <siyu.jiang81@gmail.com>
2023-03-01 16:53:30 -0500

    Move site

---
+++ scripts/loadWriteups.js
@@ -0,0 +1,20 @@
+async function readFile(name) {
+    return await (await fetch(name)).text();
+}
+
+async function loadWriteups(dirPath) {
+    var content = await readFile(dirPath);
+    var fileList = content.split(/\r?\n/);
+    const contentDiv = document.getElementById("writeupstream");
+    for (c of fileList) {
+        if (c.length == 0) continue;
+        if ((c[0] == ';') || (c[0] == '/')) continue;      //comment characters
+        var newDiv = document.createElement("div");
+        newDiv.className = "writeup";
+        newDiv.id = c.split('.').slice(0, -1).join('.');
+        newDiv.innerHTML = await readFile("./writeups/" + c);
+        contentDiv.appendChild(newDiv);
+    }
+
+    return;
+}
