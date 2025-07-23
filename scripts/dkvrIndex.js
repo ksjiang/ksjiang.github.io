7992737bf459323552265c996943b30a750bc434
Kyle <siyu.jiang81@gmail.com>
2025-07-22 21:08:56 -0400

    Improve virtual remote interface styles

--- scripts/dkvrIndex.js
+++ scripts/dkvrIndex.js
@@ -1,16 +1,18 @@
 
 function actionAwaitTransmit() {
-	document.getElementById("status").innerHTML = "Transmitter connected!";
+	document.getElementById("status").innerHTML = "Remote connected!";
 	document.getElementById("bleConnect").style.display = "none";
 	document.getElementById("tx318").style.display = "";
 	return;
 }
 
 function actionAwaitConnect() {
-	document.getElementById("status").innerHTML = "Please connect to remote.";
+	document.getElementById("status").innerHTML = "Please connect to Bluetooth remote.";
 	document.getElementById("bleConnect").style.display = "";
 	document.getElementById("tx318").style.display = "none";
 	document.getElementById("serialLbl").innerHTML = "XXXX&nbsp;XXXXX";
+	document.getElementById("serialLbl").style.fontWeight = "";
+	document.getElementById("serialLbl").style.color = "";
 	return;
 }
 
@@ -22,7 +24,7 @@ function actionConnecting() {
 }
 
 function errBLENotAvail() {
-	document.getElementById("status").innerHTML = "Web-BLE is not available!";
+	document.getElementById("status").innerHTML = "Web-BLE is not available! Switch to a different browser.";
 	document.getElementById("bleConnect").style.display = "none";
 	document.getElementById("tx318").style.display = "none";
 	return;
@@ -30,6 +32,8 @@ function errBLENotAvail() {
 
 function updateSticker() {
 	document.getElementById("serialLbl").innerHTML = `${(SER >>> 16).toString().padStart(4, '0')}&nbsp;${(SER & 0xffff).toString().padStart(5, '0')}`;
+	document.getElementById("serialLbl").style.fontWeight = "bold";
+	document.getElementById("serialLbl").style.color = "#9834e0";
 	return;
 }
 
@@ -42,6 +46,8 @@ document.addEventListener("DOMContentLoaded", function () {
 	document.getElementById("tx318").addEventListener("click", transmitCode);
 	document.getElementById("tx318").style.display = "none";
 	document.getElementById("serialLbl").innerHTML = "XXXX&nbsp;XXXXX";
+	document.getElementById("serialLbl").style.fontWeight = "";
+	document.getElementById("serialLbl").style.color = "";
 	if (isWebBLEAvail()) actionAwaitConnect(); else errBLENotAvail();
 	return;
 });
